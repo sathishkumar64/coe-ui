34981083a651a83fb9eee6995b97dfbb2a4c41f0
sathish vasu <sathishkumar64@gmail.com>
2019-03-17 09:18:33 +0530

    updated ui

--- src/main/java/com/coeui/controller/COEController.java
+++ src/main/java/com/coeui/controller/COEController.java
@@ -3,6 +3,8 @@ package com.coeui.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,10 +12,10 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.coeui.model.School;
 import com.coeui.model.Student;
+import com.coeui.service.CountryService;
 import com.coeui.service.RestClientService;
 
 import io.swagger.annotations.Api;
@@ -22,13 +24,21 @@ import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 
 @Api(value="COE-Istio Implementation", description="Operations pertaining to COE - School & Service")
-@RestController
-@RequestMapping("/api/v1/coe")
+@Controller
 public class COEController {
 
 	@Autowired
 	private RestClientService service;
 
+	@Autowired
+	private CountryService countryService;
+	
+	@RequestMapping(value="/")
+    public String notesList(Model model) {
+        model.addAttribute("countryInfo", countryService.getCountryInfo());
+        return "coe-ui";
+    }
+	
 	
 	@ApiOperation(value = "View a list of available school", response = Iterable.class)
 	@ApiResponses(value = {
@@ -37,26 +47,26 @@ public class COEController {
 	        @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
 	        @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
 	})
-	@GetMapping(path="/school/all",produces = MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping(path="/api/v1/coe/school/all",produces = MediaType.APPLICATION_JSON_VALUE)
 	public @ResponseBody Iterable<School> getAllSchool(@RequestHeader HttpHeaders headers) {		
 		return service.findAllSchools(headers);
 	}
 	
 	
 	@ApiOperation(value = "Search a school with an School Name",response = School.class)
-	@GetMapping(path="/school",produces = MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping(path="/api/v1/coe/school",produces = MediaType.APPLICATION_JSON_VALUE)
 	public @ResponseBody School findSchoolById(@RequestParam (value = "schoolName") String schoolName) {		
 		return service.findSchoolByName(schoolName);
 	}
 	
 	@ApiOperation(value = "Add a new School")
-	@PostMapping(path="/school" )
+	@PostMapping(path="/api/v1/coe/school" )
 	public @ResponseBody String saveSchool (@RequestBody School school) {		
 		return service.saveSchool(school);
 	}
 	
 	@ApiOperation(value = "Search a Student List with an School Name",response = String.class)
-	@GetMapping(path="/school/getStudentsBySchool",produces = MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping(path="/api/v1/coe/school/getStudentsBySchool",produces = MediaType.APPLICATION_JSON_VALUE)
 	public @ResponseBody String getStudentsBySchool(@RequestParam (value = "schoolName") String schoolName,@RequestHeader HttpHeaders headers) {		
 		return service.getStudentsBySchool(schoolName,headers);
 	}
@@ -74,19 +84,19 @@ public class COEController {
 	        @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
 	        @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
 	})
-	@GetMapping(path="/student/all",produces = MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping(path="/api/v1/coe/student/all",produces = MediaType.APPLICATION_JSON_VALUE)
 	public @ResponseBody Iterable<Student> findAllStudent(@RequestHeader HttpHeaders headers) {
 		return service.findAllStudent(headers);
 	}
 	
 	@ApiOperation(value = "Search a Student List with an School Name",response = String.class)
-	@GetMapping(path="/student",produces = MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping(path="/api/v1/coe/student",produces = MediaType.APPLICATION_JSON_VALUE)
 	public @ResponseBody String getStudentsBySchoolName(@RequestParam (value = "schoolName") String schoolName) {		
 		return service.getStudentsBySchoolName(schoolName);
 	}
 	
 	@ApiOperation(value = "Add a new Student")
-	@PostMapping(path="/student" )
+	@PostMapping(path="/api/v1/coe/student" )
 	public @ResponseBody String saveStudent (@RequestBody Student student) {		
 		return service.saveStudent(student);
 	}
---
+++ src/main/java/com/coeui/service/CountryService.java
@@ -0,0 +1,72 @@
+package com.coeui.service;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.coeui.model.CountryInfo;
+
+@Service
+public class CountryService {
+	
+	
+	@Autowired
+	private GoogleZoneFinder googleZoneFinder;
+	
+	
+	
+	private static final Map<String, CountryInfo> map = Collections.unmodifiableMap(new HashMap<String, CountryInfo>() {
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
+
+	{		   
+		map.put("northamerica-northeast1", new CountryInfo("Montréal, Canada","https://upload.wikimedia.org/wikipedia/commons/d/d9/Flag_of_Canada_%28Pantone%29.svg"));
+		map.put("us-central1", new CountryInfo("Council Bluffs, Iowa, USA","https://upload.wikimedia.org/wikipedia/en/a/a4/Flag_of_the_United_States.svg"));
+		map.put("us-west1", new CountryInfo("The Dalles, Oregon, USA","https://upload.wikimedia.org/wikipedia/en/a/a4/Flag_of_the_United_States.svg"));
+		map.put("us-east4", new CountryInfo("Ashburn, Virginia, USA","https://upload.wikimedia.org/wikipedia/en/a/a4/Flag_of_the_United_States.svg"));
+		map.put("us-east1", new CountryInfo("Moncks Corner, South Carolina, USA","https://upload.wikimedia.org/wikipedia/en/a/a4/Flag_of_the_United_States.svg"));
+		map.put("southamerica-east1", new CountryInfo("São Paulo, Brazil", "https://upload.wikimedia.org/wikipedia/en/0/05/Flag_of_Brazil.svg"));
+		map.put("europe-west1", new CountryInfo("St. Ghislain, Belgium", "https://upload.wikimedia.org/wikipedia/commons/6/65/Flag_of_Belgium.svg"));
+		map.put("europe-west2", new CountryInfo("London, U.K.", "https://upload.wikimedia.org/wikipedia/en/a/ae/Flag_of_the_United_Kingdom.svg"));
+		map.put("europe-west3",new CountryInfo("Frankfurt, Germany", "https://upload.wikimedia.org/wikipedia/en/b/ba/Flag_of_Germany.svg"));
+		map.put("europe-west4", new CountryInfo("Eemshaven, Netherlands","https://upload.wikimedia.org/wikipedia/commons/2/20/Flag_of_the_Netherlands.svg"));
+		map.put("asia-south1", new CountryInfo("Mumbai, India", "https://upload.wikimedia.org/wikipedia/en/4/41/Flag_of_India.svg"));
+		map.put("asia-southeast1", new CountryInfo("Jurong West, Singapore", "https://upload.wikimedia.org/wikipedia/commons/4/48/Flag_of_Singapore.svg"));
+		map.put("asia-east1", new CountryInfo("Changhua County, Taiwan","https://upload.wikimedia.org/wikipedia/commons/7/72/Flag_of_the_Republic_of_China.svg"));
+		map.put("asia-northeast1", new CountryInfo("Tokyo, Japan", "https://upload.wikimedia.org/wikipedia/en/9/9e/Flag_of_Japan.svg"));
+		map.put("australia-southeast1", new CountryInfo("Sydney, Australia", "https://upload.wikimedia.org/wikipedia/en/b/b9/Flag_of_Australia.svg"));
+			
+	 }});
+	
+	
+	
+	
+	public List<CountryInfo> getCountryInfo(){	
+		
+		ArrayList<CountryInfo> list=new ArrayList<>();
+		
+		String flag=null;
+		
+		CountryInfo countryInfo =null;
+		
+		ArrayList<String> deployedZoneList =googleZoneFinder.printInstances();
+		
+		for (String location : deployedZoneList) {
+			
+			flag=location.substring(0, location.lastIndexOf('-'));
+			
+			countryInfo=map.get(flag);
+			
+			list.add(countryInfo);
+		}
+	
+		return list;
+	}
+}
