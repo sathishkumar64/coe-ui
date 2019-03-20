62161f4e332088d14ba18e698280f60e1d3cc042
sathish vasu <sathishkumar64@gmail.com>
2019-03-20 15:44:54 +0530

    werwerw

--- src/main/java/com/coeui/model/CountryInfo.java
+++ src/main/java/com/coeui/model/CountryInfo.java
@@ -1,44 +1,32 @@
 package com.coeui.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class CountryInfo {
-	
+
 	private String location;
 	private String flag;
-	
-	
-	public CountryInfo(String location, String flag) {
-		super();
-		this.location = location;
-		this.flag = flag;
-	}
 
+	public CountryInfo() {
+		super();
+	}
 
 	public String getLocation() {
 		return location;
 	}
 
-
 	public void setLocation(String location) {
 		this.location = location;
 	}
-
 
 	public String getFlag() {
 		return flag;
 	}
 
-
 	public void setFlag(String flag) {
 		this.flag = flag;
 	}
 
-
-	@Override
-	public String toString() {
-		return "CountryInfo [location=" + location + ", flag=" + flag + "]";
-	}
-	
-	
-	
 
 }
